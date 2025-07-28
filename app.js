8d250b97d01103d18de828add6843b666d269e60
SAFO <ltaief.oswa1@gmail.com>
2025-07-28 11:11:38 +0200

    add the line

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 console.log("coucou");
 console.log(3+2)
 
+alert(" Bienvenue sur ma page !");
+
+
+let name = prompt("Quel est ton nom?");
+
+alert ( "Coucou " +name)
+let age = prompt(" Quel est ton âge ? ") 
+if  ( age > 14 ){ alert("Bienvenue à Jump In Tech !")
+} else { alert("Revient dans quelques années <3") 
+}
+
+
+let MOONLIGHT = document.getElementById ("TITRE");
