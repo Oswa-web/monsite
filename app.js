f6bba96289531cd61d34312c4069b7ad237e80ab
SAFO <ltaief.oswa1@gmail.com>
2025-07-29 15:22:21 +0200

    make all changes

--- app.js
+++ app.js
@@ -1,23 +1,2 @@
-<<<<<<< HEAD
-console.log("coucou");
-console.log(3+2)
 
-alert(" Bienvenue sur ma page !");
-
-
-let name = prompt("Quel est ton nom?");
-
-let montitre = document.getElementById ("titre");
-montitre.innerText = "MOONLIGHT DE "+ name;
-
-if (name == "Oswa") {
-	alert("COMME MOI SUIUIUUIIUIUIUIUIUIU LETTTSS GOO");
-	montitre.style.color = "#1a44af" ; 
-} else { 
-montitre.style.color = "#FCEF91"
-}
-
-alert ( "Coucou " +name)
-=======
 let MOONLIGHT = document.getElementById ("TITRE");
->>>>>>> 13932e08ba252c646eef29dc715fd47f9c5c4c6a
